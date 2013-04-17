24d72a29a985eb500492653193da70b1f219d6e7
goldquest <tjwelde@gmail.com>
2013-04-17 02:30:22 +0200

    Toggle Header

--- src/HUBerlin/EPLiteProBundle/Resources/public/js/base.js
+++ src/HUBerlin/EPLiteProBundle/Resources/public/js/base.js
@@ -6,4 +6,22 @@ $(function() {
     	langform.submit();
 	});
 	
+    $('#toggle-header').click(function(e){
+        e.preventDefault();
+        var obj = $(this);
+        if(obj.hasClass('hidden')) {
+        	obj.removeClass('hidden');
+            $('#header').animate({
+                top: '0',
+                marginBottom: '0',
+            });
+        }
+        else {
+        	obj.addClass('hidden');
+            $('#header').animate({
+                top: '-110px',
+                marginBottom: '-100px'
+            });
+        }
+    });
 });
