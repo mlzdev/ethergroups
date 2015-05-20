db97417fb2bed3dd269969299d4425a2dcafba21
goldquest <tjwelde@gmail.com>
2015-05-20 15:18:15 +0200

    added a command, which removes old invitations

---
+++ src/Ethergroups/MainBundle/Command/removeOldInvitationsCommand.php
@@ -0,0 +1,49 @@
+<?php
+namespace Ethergroups\MainBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Entities\Users;
+
+class RemoveOldInvitationsCommand extends ContainerAwareCommand {
+    
+    protected function configure()
+    {
+        $this
+        ->setName('Ethergroups:removeOldInvitations')
+        ->setDescription('Removes Invitations, which are older then x days')
+        ->addArgument('days', InputArgument::OPTIONAL, 'How old an invitation be? (In days / Default: 21)')
+        //->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters')
+        ;
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Get all neccessary services
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        // Get all invitations
+        $invitations = $em->getRepository('EthergroupsMainBundle:Invitation')->findAll();
+
+        $days = $input->getArgument('days');
+
+        $days = (isset($days))?$days:21;
+
+        $daysAgo = strtotime("today midnight -$days days");
+
+        foreach ($invitations as $invitation) {
+            if($invitation->getCreated() < $daysAgo) {
+                $output->writeln(
+                    'Invitation for user: '.$invitation->getUser()->getUid().' and group: '.$invitation->getGroup()->getGroupid().' removed'
+                );
+                $em->remove($invitation);
+            }
+        }
+        $em->flush();
+        
+        $output->writeln('successful');
+    }
+}
