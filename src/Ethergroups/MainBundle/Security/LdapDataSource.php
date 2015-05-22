68161489e1f0914028b736680dd33b67aa5f47c5
goldquest <tjwelde@gmail.com>
2015-05-22 21:06:02 +0200

    also search the givenname and mail

--- src/Ethergroups/MainBundle/Security/LdapDataSource.php
+++ src/Ethergroups/MainBundle/Security/LdapDataSource.php
@@ -72,7 +72,7 @@ class LdapDataSource
 	        $result = $this->searchRecords('(|('.$this->ldapUserAttribute . "=" . $user.')(sn='.$user.')(cn='.$user.')(givenName='.$user.')(mail='.$user.'))', array("uid", "sn", "cn", "mail", "givenName"), $resourceLink);
 	    }
 	    else {
-	        $result = $this->searchRecords('(|('.$this->ldapUserAttribute . "=" . $user.')(cn='.$user.'*)(sn='.$user.'*))', array("uid", "sn", "cn", "givenName"), $resourceLink, 15, 1);
+	        $result = $this->searchRecords('(|('.$this->ldapUserAttribute . "=" . $user.')(cn='.$user.'*)(sn='.$user.'*)(givenName='.$user.'*)(mail='.$user.'*))', array("uid", "sn", "cn", "mail", "givenName"), $resourceLink, 5, 1);
 	    }
 	    
 	    if(!$returnall) {
